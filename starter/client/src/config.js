5958305c91802aa76a9e41d168175a8b65cd090a
huunguyen2107 <huunguyen2107@gmail.com>
2024-07-03 23:16:38 +0700

    4th

--- starter/client/src/config.js
+++ starter/client/src/config.js
@@ -1,4 +1,4 @@
 export const AUTH0_DOMAIN='dev-ul582kmak2uybbmj.us.auth0.com'
-export const AUTH0_CLIENT_ID='Yj9n9IvfR236DbrEO6mfmGK2eqiDymss'
+export const AUTH0_CLIENT_ID='y7eJ8S1q4SMRi9buCzhZ7AFWutCiHzyl'
 export const AUTH0_AUDIENCE_ENDPOINT='https://dev-ul582kmak2uybbmj.us.auth0.com/api/v2/'
 export const API_ENDPOINT='https://i6nvrfdfu9.execute-api.us-east-1.amazonaws.com/dev'
